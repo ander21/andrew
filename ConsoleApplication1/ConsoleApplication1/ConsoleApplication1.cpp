94c2e530941968319e0352a87259a45fa70d76fd
user <user@A34COMP12>
2017-12-16 18:00:50 +0200

    add var

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+
+    return 0;
+}
+
